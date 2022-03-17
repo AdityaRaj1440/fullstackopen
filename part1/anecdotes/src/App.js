8570a939d44f4d9f54bb3937446032fb0ccdd61e
AdityaRaj1440 <raj.aaditya1440@gmail.com>
2022-03-17 18:27:24 +0530

    anecdotes part1 done

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react'
+
+const App = () => {
+
+  let ind= 0
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
+  
+  let a= Array(anecdotes.length).fill(0)
+  const [vote, setVote] = useState(a)
+  const [selected, setSelected] = useState(0)
+  const [maxIndex, setMaxIndex] = useState(0)
+
+  useEffect(() => {
+    
+    
+  })
+
+
+     console.log("vote: ",vote);
+
+     const changeVote = () => {
+        console.log("Button clicked")
+      let arr= vote
+      arr[selected]+=1
+      let max=0
+    for(let i=0; i<vote.length; i++)
+    {
+      if(max<arr[i])
+      {
+        max= arr[i]
+        ind=i
+      }
+    }
+      setVote([...arr])
+      setMaxIndex(ind)
+      console.log(arr)
+      console.log("votearr:",vote)
+    
+    }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <br/>
+      {anecdotes[selected]}
+      <br/>
+      has {vote[selected]} votes
+      <br/>
+      <button onClick= {() => changeVote()}>vote</button>
+      <button onClick={() => setSelected(Math.floor(Math.random()*100)%anecdotes.length)}>next anecdote</button>
+
+      <h1>Anecdote with highest votes</h1>
+      {anecdotes[maxIndex]}
+      <br/>
+      {vote[maxIndex]}
+    </div>
+  )
+
+
+}
+
+export default App
